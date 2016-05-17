85122ac3c7b11f79229ff98feb4ef6cf3ede77c3
guanxyz <guanxyz82@gmail.com>
2016-05-17 15:21:51 -0600

    Updated the darwin branch

--- runmpas.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-# You MUST go to your app directory directly with ocean binary at the same level
-cd /home/user/LANL/app/mpas_240km
-mpiexec -machinefile /home/user/LANL/machinefile /home/user/LANL/app/mpas_240km/ocean_forward_model
