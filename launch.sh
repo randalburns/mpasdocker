98c12a64124b1c2da6336b43c3ae348e540e85a3
guanxyz <guanxyz82@gmail.com>
2016-05-18 14:14:36 -0600

    Changes in launch.sh

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## make the run directory belong to vpic
-mkdir -p /mnt/mpasrun
+#mkdir -p /mnt/mpasrun
 cp -r /home/mpas/* /mnt/mpasrun/
 #mkdir -p /mnt/vpicrun/vpic.bin
 #mkdir -p /mnt/vpicrun/vpicrun1
 #mkdir -p /mnt/vpicrun/vpicrun2
-chown -R mpas:mpas /mnt/mpascrun/*
+chown -R mpas:mpas /mnt/mpasrun/*
 #
 # run the vpic code  on master
 su mpas -c /home/mpas/LANL/dockerrunmpas.sh
