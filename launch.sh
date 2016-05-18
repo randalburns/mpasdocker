2fcb513a6fb17b14b69860d17e6f2b7d5bfd1022
guanxyz <guanxyz82@gmail.com>
2016-05-18 12:32:31 -0600

    Fixed bugs

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## make the run directory belong to vpic
-cp -r /home/mpas/* /mnt/mpasrun
+cp -r /home/mpas/* /mnt/mpasrun/
 #mkdir -p /mnt/vpicrun/vpic.bin
 #mkdir -p /mnt/vpicrun/vpicrun1
 #mkdir -p /mnt/vpicrun/vpicrun2
--- run_mpas.sh
+++ run_mpas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -i --net=host -v /vmshare:/mnt/mpasrun mpas /home/mpas/launch.sh
+docker run -i --net=host -v /vmshare:/mnt/mpasrun mpas /home/mpas/LANL/launch.sh
