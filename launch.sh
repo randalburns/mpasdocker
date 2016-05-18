523221c8847118fe9c14eb894a9b9ed3e43454fc
guanxyz <guanxyz82@gmail.com>
2016-05-18 13:30:37 -0600

    Change the directory of run-directory

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## make the run directory belong to vpic
+mkdir -p /mnt/mpasrun
 cp -r /home/mpas/* /mnt/mpasrun/
 #mkdir -p /mnt/vpicrun/vpic.bin
 #mkdir -p /mnt/vpicrun/vpicrun1
@@ -8,4 +9,4 @@ cp -r /home/mpas/* /mnt/mpasrun/
 chown -R mpas:mpas /mnt/mpascrun/*
 #
 # run the vpic code  on master
-su mpas -c /home/mpas/dockerrunmpas.sh
+su mpas -c /home/mpas/LANL/dockerrunmpas.sh
